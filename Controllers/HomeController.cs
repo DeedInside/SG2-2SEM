d61c307fb8fb7bb437a56bb0e7e93666a39e2db8
Пауль Сергей Александрович <paul_s_a@mail.ru>
2024-02-13 14:48:15 +0700

    Добавьте файлы проекта.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SG2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Short()
+        {
+            return View();
+        }
+    }
+}
